a3cece10c96225238e887ffb0ec48df19684cd22
MariHovhannisyan <marihovhannisyan6@gmail.com>
2025-04-12 10:43:12 +0400

    Updated Kahn's

--- Kahn's.py
+++ Kahn's.py
@@ -1,25 +1,27 @@
-from collections import deque, defaultdict
+from collections import deque
 
-def kahn_topological_sort(graph):
-    in_degree = {node: 0 for node in graph}
-    for neighbors in graph.values():
+def kahn(adj_list):
+    in_degree = {node: 0 for node in adj_list}
+    for neighbors in adj_list.values():
         for neighbor in neighbors:
             in_degree[neighbor] += 1
 
-    queue = deque([node for node, degree in in_degree.items() if degree == 0])
-    result = []
+    #nodes having zero in-degree
+    zero_in_degree = deque([node for node, deg in in_degree.items() if deg == 0])
+    sorted_order = []
 
-    while queue:
-        node = queue.popleft()
-        result.append(node)
-        for neighbor in graph[node]:
+    while zero_in_degree:
+        current = zero_in_degree.popleft()
+        sorted_order.append(current)
+        for neighbor in adj_list[current]:
             in_degree[neighbor] -= 1
             if in_degree[neighbor] == 0:
-                queue.append(neighbor)
+                zero_in_degree.append(neighbor)
 
-    return result
+    return sorted_order
 
-graph = {
+
+dag_graph = {
     'A': ['B', 'C', 'D'],
     'B': ['J'],
     'C': ['F', 'I'],
@@ -33,6 +35,5 @@ graph = {
     'K': []
 }
 
-result = kahn_topological_sort(graph)
-print(result)
-
+order = kahn(dag_graph)
+print("Topological Order:", order)
